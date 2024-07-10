27bef7b15d7e1b098c6e887699a05e0af8aae01f
tonyfanelli317 <72234140+tonyfanelli317@users.noreply.github.com>
2024-07-10 14:33:32 -0500

    Fixed index/app.js

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const App = () => {
+    return(<div>
+        <h1>Hello World!</h1>
+    </div>);
+};
--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-return(<div>
-    <h1>Hello World!</h1>
-    </div>);
